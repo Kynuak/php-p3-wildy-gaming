5d92ca3c6aa5bee691644691ded185d1d476090e
Sajid Salhi <sajid.s35400@gmail.com>
2024-01-15 14:33:30 +0100

    essai jeu space invaders

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -1,18 +1,21 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
-
 class GameController extends AbstractController
 {
- #[Route('/pacman', name: 'jeux-pacman')]
- public function index(): Response
- {
-      return $this->render('pacman.html.twig');
- }
-
+    #[Route('/pacman', name: 'jeux-pacman')]
+    public function index(): Response
+    {
+         return $this->render('pacman.html.twig');
+    }
+    #[Route('/space_invender', name: 'space_invender')]
+    public function invender(): Response
+    {
+         return $this->render('space_invender.html.twig');
+    }
 }
